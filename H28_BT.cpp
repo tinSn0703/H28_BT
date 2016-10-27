cd2bcd0d4ec9e97e896e5c714145bf5af1442119
tinSn0703 <condensa1952@gmail.com>
2016-10-27 17:15:31 +0900

    6.0.0
    Bluetoothとの通信用の関数に絞る、u_fancの中身は使わない。#defineの色々諸々など

--- H28_BT.cpp
+++ H28_BT.cpp
@@ -1,365 +1,143 @@
 ﻿
 #pragma once
 
-inline int
-strcmp
+inline BOOL
+F_Str_comp
 (
-	const char *_arg_str_f,
-	char *_arg_str
+	const char	_arg_str_base[],
+	char		_arg_str_comp[]
 )
 {
-	for (usint i = 0; _arg_str_f[i] != '\0'; i++)
-	{
-		if (_arg_str_f[i] != _arg_str[i])
-		{
-			return 1;
-		}
-	}
+	for (usint i = 0; _arg_str_base[i] != '\0'; i++)	{	if (_arg_str_base[i] != _arg_str_comp[i])	return FALSE;	}
 	
-	return 0;
+	return TRUE;
 }
 
 inline char *
-F_str_copy
+F_Str_copy
 (
-	char _arg_str_base[],
-	const char _arg_str[]
+	char		_arg_str_base[],
+	const char	_arg_str_copy[]
 )
 {
-	for (uint i = 0; (_arg_str_base[i] == '\0') || (_arg_str[i] == '\0'); i++)
-	{
-		_arg_str_base[i] = _arg_str[i];
-	}
+	for (uint i = 0; (_arg_str_base[i] == '\0') || (_arg_str_copy[i] == '\0'); i++)	{	_arg_str_base[i] = _arg_str_copy[i];	}
 	
 	return _arg_str_base;
 }
 
-void 
-F_Set_bt (E_UART_ADDR _arg_uart_addr)
+inline void 
+F_Str_add
+(
+	char		_arg_str_base[], 
+	const char	_arg_str_add[]
+)
 {
-	DDRD	|= (__CTS__ | __RSE__);
-	PORTD	|= __RTS__;
+	usint i = 0;
+	usint j = 0;
 	
-	F_uart_set_detail(_arg_uart_addr, 10, EU_NON);
+	while(_arg_str_base[i] != '\0')
+	{
+		i++;
+	}
 	
-	F_In_comp_bt(_arg_uart_addr, "\r\nOK\r\n");
+	while(_arg_str_add[j] != '\0')
+	{
+		_arg_str_base[i] = _arg_str_add[j];
+		
+		i++;	j++;
+	}
+	
+	_arg_str_base[i] = '\0';
 }
 
-void 
-F_Out_bt 
+inline void 
+F_BT_Out
 (
-	E_UART_ADDR _arg_uart_addr_t, 
+	E_UART_ADDR _arg_uart_addr_t,
 	const char _arg_data_out[]
 )
 {
 	for (usint i = 0; _arg_data_out[i] != '\0'; i++)
 	{
-		while (__RTS_READ__);
+		while(__RTS_READ__);
 		
-		F_uart_out_8(_arg_uart_addr_t, _arg_data_out[i]);
+		while ( ! (__UCSRA_F(_arg_uart_addr_t) & (1 << UDRE)));
+		
+		__UDR_F(_arg_uart_addr_t) = _arg_data_out[i];
 	}
 }
 
-char *
-F_In_bt 
+inline void 
+F_BT_In
 (
-	E_UART_ADDR _arg_uart_addr_r, 
-	char _arg_data_in[]
+	E_UART_ADDR _arg_uart_addr_r,
+	char		_arg_re_data_in[]
 )
-{
+{	
 	usint i = 0;
 	
 	while (1)
 	{
 		__CTS_LOW___;
 		
-		T_DATA_8 _temp_data_in = 0;
+		while ( ! (__UCSRA_F(_arg_uart_addr_r) & (1 << RXC)));
 		
-		E_UART_FLAG _flag = F_uart_in_8(_arg_uart_addr_r, &_temp_data_in);
+		 char _temp = __UDR_F(_arg_uart_addr_r);
 		
 		__CTS_HIGH__;
 		
-		if (_flag != EU_ERROR)
+		if ((__UCSRA_F(_arg_uart_addr_r) & ((1 << FE) | (1 << DOR) | (1 << UPE))) == 0x00)
 		{
-			_arg_data_in[i] = _temp_data_in;
+			_arg_re_data_in[i] = _temp;
 			
-			if ((_arg_data_in[i] == '\n') && (1 < i))	break;
+			if ((_arg_re_data_in[i] == '\n') && (i > 1))
+			{
+				_arg_re_data_in[i + 1] = '\0';
+				
+				break;
+			}
 			
 			i++;
 		}
 	}
-	
-	_arg_data_in[i + 1] = '\0';
-	
-	return _arg_data_in;
 }
 
-char *
-F_In_bt 
-(
-	E_UART_ADDR _arg_uart_addr_r, 
-	char _arg_data_in[], 
-	const char _arg_data_comp[]
-)
-{
-	do 
-	{
-		F_In_bt(_arg_uart_addr_r, _arg_data_in);
-		
-	} while (strcmp(_arg_data_comp, _arg_data_in));
-	
-	return _arg_data_in;
-}
-
-void 
-F_In_comp_bt
+inline void 
+F_BT_In_comp
 (
 	E_UART_ADDR _arg_uart_addr_r,
-	const char _arg_data_comp[]
+	char		_arg_re_data_in[],
+	const char	_arg_data_comp[]
+)
+{
+	do
+	{
+		F_BT_In(_arg_uart_addr_r, _arg_re_data_in);
+		
+	} while (F_Str_comp(_arg_data_comp, _arg_re_data_in) == FALSE);
+}
+
+inline void 
+F_BT_In_comp
+(
+	E_UART_ADDR _arg_uart_addr_r,
+	const char	_arg_data_comp[]
 )
 {
 	char _data_in[40] = {};
 	
 	do
 	{
-		F_In_bt(_arg_uart_addr_r, _data_in);
+		F_BT_In(_arg_uart_addr_r, _data_in);
 		
-	} while (strcmp(_arg_data_comp, _data_in));
+	} while (F_Str_comp(_arg_data_comp, _data_in) == FALSE);
 }
 
-#if defined(_AKILCD_H_)
-
-char * 
-F_Set_bt_addr
-(
-	E_BT_NUM _arg_num_bt,
-	char _arg_bt_addr[],
-	char _arg_char_num
-)
+inline void 
+F_BT_Cut ()
 {
-	switch (_arg_num_bt)
-	{
-		case EB_BT0:	F_str_copy(_arg_bt_addr ,"000190EAD4B5"); _arg_char_num = '1';	break;
-		case EB_BT1:	F_str_copy(_arg_bt_addr ,"000190EAD491"); _arg_char_num = '2';	break;
-		case EB_BT2:	F_str_copy(_arg_bt_addr ,"000190E53F09"); _arg_char_num = '3';	break;
-		case EB_BT3:	F_str_copy(_arg_bt_addr ,"000190EAD491"); _arg_char_num = '4';	break;
-		case EB_BT4:	F_str_copy(_arg_bt_addr ,"000190EAD49A"); _arg_char_num = '5';	break;
-		case EB_BT5:	F_str_copy(_arg_bt_addr ,"000190E53A7B"); _arg_char_num = '6';	break;
-		case EB_BT6:	F_str_copy(_arg_bt_addr ,"000190EB1273"); _arg_char_num = '7';	break;
-		case EB_BT7:	F_str_copy(_arg_bt_addr ,"000190EB1416"); _arg_char_num = '8';	break;
-		case EB_BT8:	F_str_copy(_arg_bt_addr ,"000190EB105D"); _arg_char_num = '9';	break;
-		case EB_BT9:	F_str_copy(_arg_bt_addr ,"000190EB1057"); _arg_char_num = 'A';	break;
-		case EB_BTA:	F_str_copy(_arg_bt_addr ,"000190EB105A"); _arg_char_num = 'B';	break;
-		case EB_BTB:	F_str_copy(_arg_bt_addr ,"000190EB1052"); _arg_char_num = 'C';	break;
-		case EB_BTC:	F_str_copy(_arg_bt_addr ,"000190EB1050"); _arg_char_num = 'D';	break;
-		case EB_BTD:	F_str_copy(_arg_bt_addr ,"000190EB105C"); _arg_char_num = 'E';	break;
-		case EB_BTE:	F_str_copy(_arg_bt_addr ,"000190EB104F"); _arg_char_num = 'F';	break;
-		case EB_BTF:	F_str_copy(_arg_bt_addr ,"000190EB104E"); _arg_char_num = '0';	break;
-		default:																		break;
-	}
+	__CTS_LOW___;
 	
-	return _arg_bt_addr;
-}
-
-#else
-
-char * 
-F_Set_bt_addr
-(
-	E_BT_NUM _arg_num_bt,	
-	char _arg_bt_addr[]
-)
-{
-	switch (_arg_num_bt)
-	{
-		case EB_BT0:	F_str_copy(_arg_bt_addr ,"000190EAD4B5");	break;
-		case EB_BT1:	F_str_copy(_arg_bt_addr ,"000190EAD491");	break;
-		case EB_BT2:	F_str_copy(_arg_bt_addr ,"000190E53F09");	break;
-		case EB_BT3:	F_str_copy(_arg_bt_addr ,"000190EAD491");	break;
-		case EB_BT4:	F_str_copy(_arg_bt_addr ,"000190EAD49A");	break;
-		case EB_BT5:	F_str_copy(_arg_bt_addr ,"000190E53A7B");	break;
-		case EB_BT6:	F_str_copy(_arg_bt_addr ,"000190EB1273");	break;
-		case EB_BT7:	F_str_copy(_arg_bt_addr ,"000190EB1416");	break;
-		case EB_BT8:	F_str_copy(_arg_bt_addr ,"000190EB105D");	break;
-		case EB_BT9:	F_str_copy(_arg_bt_addr ,"000190EB1057");	break;
-		case EB_BTA:	F_str_copy(_arg_bt_addr ,"000190EB105A");	break;
-		case EB_BTB:	F_str_copy(_arg_bt_addr ,"000190EB1052");	break;
-		case EB_BTC:	F_str_copy(_arg_bt_addr ,"000190EB1050");	break;
-		case EB_BTD:	F_str_copy(_arg_bt_addr ,"000190EB105C");	break;
-		case EB_BTE:	F_str_copy(_arg_bt_addr ,"000190EB104F");	break;
-		case EB_BTF:	F_str_copy(_arg_bt_addr ,"000190EB104E");	break;
-		default:													break;
-	}
-	
-	return _arg_bt_addr;
-}
-
-#endif
-
-BOOL  
-F_Connect_bt_slave 
-(
-	E_UART_ADDR _arg_uart_addr,
-	E_BT_NUM _arg_num_bt
-)
-{
-	char _bt_master_addr[__BT_ADDR_BYTE__] = {};
-	
-#if defined(_AKILCD_H_)
-	
-	Lcd_put_str(0x40, "Now setting     ");
-	
-	char _bt_master_num = 0;
-
-	F_Set_bt_addr(_arg_num_bt, _bt_master_addr, _bt_master_num);
-	
-	char _state_ing[15] = "Connecting_0x ";	_state_ing[13] = _bt_master_num;
-	char _state_ed [15] = "Connected_0x  ";	_state_ed [12] = _bt_master_num;
-	
-	Lcd_put_str(0x40,_state_ing);
-	
-#else
-	
-	F_Set_bt_addr(_arg_num_bt, _bt_master_addr);
-	
-#endif
-	
-	char CONNECTING[] = "\r\n+CONNECTING=123456789abc\r\n";
-	char CONNECTED[]  = "\r\n+CONNECTED=123456789abc\r\n";
-	
-	for (usint i = 0; i < __BT_ADDR_BYTE__; i++)
-	{
-		CONNECTING[14 + i] = _bt_master_addr[i];
-		CONNECTED [13 + i] = _bt_master_addr[i];
-	}
-	
-	go_re_connect:
-	
-	F_Out_bt(_arg_uart_addr, "AT+CONSLAVE=1,0\r\n");
-	
-	F_In_comp_bt(_arg_uart_addr, "\r\nACK\r\n");
-	
-	char _data_in[40] = {};
-	
-	do
-	{
-		F_In_bt(_arg_uart_addr, _data_in);
-		
-		if (strcmp("\r\nTIMEOUT\r\n",_data_in) == 0)	goto go_re_connect;
-		
-	} while (strcmp(CONNECTING,_data_in) != 0);
-	
-	F_In_comp_bt(_arg_uart_addr, "\r\n+ACCEPT?\r\n");
-	
-	F_Out_bt(_arg_uart_addr, "AT+RESCON=1\r\n");
-	
-	F_In_comp_bt(_arg_uart_addr, CONNECTED);
-	
-	F_In_comp_bt(_arg_uart_addr, "\r\nOK\r\n");
-	
-#ifdef _AKILCD_H_
-	Lcd_put_str(0x40,_state_ed);
-#endif
-	
-	return TRUE;
-}
-
-BOOL  
-F_Connect_bt_master 
-(
-	E_UART_ADDR _arg_uart_addr, 
-	E_BT_NUM _arg_num_bt
-)
-{
-	char _bt_slave_addr[__BT_ADDR_BYTE__] = {};
-	
-#if defined(_AKILCD_H_)
-	
-	char _bt_slave_num = 0;
-
-	Lcd_put_str(0x40, "Now setting     ");
-
-	F_Set_bt_addr(_arg_num_bt, _bt_slave_addr, _bt_slave_num);
-	
-	char _state_ing[15] = "Connecting_0x ";
-	char _state_ed [15] = "Connected_0x  ";
-	
-	_state_ing[13] = _bt_slave_num;
-	_state_ed [12] = _bt_slave_num;
-	
-	Lcd_put_str(0x40,_state_ing);
-	
-#else
-	
-	F_Set_bt_addr(_arg_num_bt, _bt_slave_addr);
-	
-#endif
-	
-	char CONMASTER[]  = "AT+CONMASTER=1,000190123456\r\n";
-	char CONNECTED[]  = "\r\n+CONNECTED=000190123456\r\n";
-	
-	for (usint i = 0; i < __BT_ADDR_BYTE__; i++)
-	{
-		CONMASTER [15 + i] = _bt_slave_addr[i];
-		CONNECTED [13 + i] = _bt_slave_addr[i];
-	}
-	
-	re_connect:
-	
-	F_Out_bt(_arg_uart_addr, CONMASTER);
-	
-	F_In_comp_bt(_arg_uart_addr, "\r\nACK\r\n");
-	
-	char _in_data[40] = {};
-	
-	do
-	{
-		F_In_bt(_arg_uart_addr, _in_data);
-		
-		if (strcmp("\r\nTIMEOUT\r\n",_in_data) == 0)	goto re_connect;
-		
-	} while (strcmp(CONNECTED,_in_data) != 0);
-	
-	F_In_comp_bt(_arg_uart_addr, "\r\nOK\r\n");
-	
-#ifdef _AKILCD_H_
-	Lcd_put_str(0x40,_state_ed);
-#endif
-
-	return TRUE;
-}
-
-void 
-F_Re_Connect_bt_slave
-(
-	E_UART_ADDR _arg_uart_addr, 
-	E_BT_NUM _arg_num_bt
-)
-{
-	F_Cut_Connect_bt();
-	
-	F_In_comp_bt(_arg_uart_addr, "\r\nOK\r\n");
-	
-	F_Re_Connect_bt_slave(_arg_uart_addr, _arg_num_bt);
-}
-
-void
-F_Re_Connect_bt_master
-(
-	E_UART_ADDR _arg_uart_addr, 
-	E_BT_NUM _arg_num_bt
-)
-{
-	F_Cut_Connect_bt();
-	
-	F_In_comp_bt(_arg_uart_addr, "\r\nOK\r\n");
-	
-	F_Re_Connect_bt_master(_arg_uart_addr, _arg_num_bt);
-}
-
-void 
-F_Cut_Connect_bt ()
-{
 	__RSE_LOW___;
 	_delay_ms(15);
 
--- H28_BT.h
+++ H28_BT.h
@@ -1,10 +1,42 @@
 
-#pragma once
+/******************************************************************************
+ H28 10 / 23 (Sun)
+・改造案
+	・C_BT_304とその派生先を削除。全て関数に変える。
+	・__CTS__などを#ifndefを用いることで、プロジェクトによって調整可能にする。
+	・C_BTの内部の大幅な見直し。
+		・__RTS__のTimerでのチェックを行わないメンバの追加。-> めんどいからやっぱりいい。
+		・受信フラグのTimerでのチェックを行わないメンバの追加。-> 同上
+		・割り込み処理に対する工夫。
+		・どうせ、戻り値使ってないし、戻り値でエラー吐かせる。
+		・メンバ変数の__CTS__などを削除し、外部も#defineに置き換える。
+	・C_BTの専用の関数の作成。ROBOBAとBlueMasterをこれで使い分ける。
+	・ピンを#defineにすることで、C_BT外部でも分割した関数でも同じように用いることができるようにする。
+	
+ ・改造点
+	・C_BTのCTS用などのメンバ変数を削除。外部の#defineを用いる構成に。
+	・__CTS__など、回路によって変更する部分を#ifndefを用いてプロジェクトごとに変更可能に。
+	・__XXX_ADDR__系を追加して、変更可能箇所を減らし、負担とミスの軽減。
+	・__XXX__系でシフトしていたが、使い勝手が悪いのでシフトした__XXX_BIT__系を用意。
+	・__XXX_READ__系はBOOL型に変換。
+	・新たに__XXX_SET__系を追加。DDRへの入力ミスを無くす。
+	・C_BT::Out()とC_BT::In()にBOOL型の戻り値を追加。これで、エラーのチェックが可能。
+	
+ H28 10 / 24 (Mon)
+	色々やってみたけど、原因不明のバグが発生しました。なんか、コントローラからのデータのデータを受け取
+　れませんでした。確認してみるとUART自体が機能してなさそう。TIMERが効かない。単体では普通に動いた。でも、
+　BTのと合わせると機能しなくなる。ちょっと意味が分からない。fuck
+　　C_BTが関係あるかは不明。C_BT用関数自体は問題なかった(消したけど)。このファイル群に置くプログラムは、
+　BTとの通信用のものだけにしておきます。あまりよくばらないほうがいいのかねえ。fuck	
+	
+******************************************************************************/
 
-//#define USED_H28_AM_164 1
+#pragma once
 
 #include <H28_AVR/H28_AVR.h>
 #include <string.h>
+
+/************************************************************************/
 
 #define __BT_DATA_NUM__		4
 #define __BT_ADDR_BYTE__	12
@@ -12,104 +44,169 @@
 #define BT_DATA_NUM		4
 #define BT_ADDR_BYTE	12
 
-#define __CTS__ (1 << PD4)
-#define __RTS__ (1 << PD5)
-#define __RSE__ (1 << PD6)
+#ifndef __CTS_ADDR__
+#	define __CTS_ADDR__ EI_PORTD
+#endif
+#ifndef __RTS_ADDR__
+#	define __RTS_ADDR__ EI_PORTD
+#endif
+#ifndef __RSE_ADDR__
+#	define __RSE_ADDR__ EI_PORTD
+#endif
 
-#define __CTS_PIN__ PIND
-#define __RTS_PIN__ PIND
-#define __RSE_PIN__ PIND
+#ifndef __CTS__
+#	define __CTS__ EI_IO4
+#endif
+#ifndef __RTS__
+#	define __RTS__ EI_IO5
+#endif
+#ifndef __RSE__
+#	define __RSE__ EI_IO6
+#endif
 
-#define __CTS_PORT__ PORTD
-#define __RTS_PORT__ PORTD
-#define __RSE_PORT__ PORTD
+#define __CTS_BIT__ (1 << __CTS__)
+#define __RTS_BIT__ (1 << __RTS__)
+#define __RSE_BIT__ (1 << __RSE__)
 
-#define __CTS_DDR__ DDRD
-#define __RTS_DDR__ DDRD
-#define __RSE_DDR__ DDRD
+#define __CTS_PIN__ _SFR_IO8(__CTS_ADDR__ + 0)
+#define __RTS_PIN__ _SFR_IO8(__RTS_ADDR__ + 0)
+#define __RSE_PIN__ _SFR_IO8(__RSE_ADDR__ + 0)
 
-#define __CTS_READ__ (__CTS_PIN__ & __CTS__)
-#define __RTS_READ__ (__RTS_PIN__ & __RTS__)
-#define __RSE_READ__ (__RSE_PIN__ & __RSE__)
+#define __CTS_DDR__ _SFR_IO8(__CTS_ADDR__ + 1)
+#define __RTS_DDR__ _SFR_IO8(__RTS_ADDR__ + 1)
+#define __RSE_DDR__ _SFR_IO8(__RSE_ADDR__ + 1)
 
-#define __RTS_READ_T__ ((__RTS_PIN__ & __RTS__) != 0x00)
-#define __RTS_READ_F__ ((__RTS_PIN__ & __RTS__) == 0x00)
+#define __CTS_PORT__ _SFR_IO8(__CTS_ADDR__ + 2)
+#define __RTS_PORT__ _SFR_IO8(__RTS_ADDR__ + 2)
+#define __RSE_PORT__ _SFR_IO8(__RSE_ADDR__ + 2)
 
-#define __CTS_HIGH__ (__CTS_PORT__ |=  __CTS__)
-#define __CTS_LOW___ (__CTS_PORT__ &= ~__CTS__)
+#define __CTS_SET__ (__CTS_DDR__ |=  __CTS_BIT__)
+#define __RTS_SET__ (__RTS_DDR__ &= ~__RTS_BIT__)
+#define __RSE_SET__ (__RSE_DDR__ |=  __RSE_BIT__)
 
-#define __RSE_HIGH__ (__RSE_PORT__ |=  __RSE__)
-#define __RSE_LOW___ (__RSE_PORT__ &= ~__RSE__)
+#define __CTS_READ__ ((BOOL)((__CTS_PIN__ >> __CTS__) & 1))
+#define __RTS_READ__ ((BOOL)((__RTS_PIN__ >> __RTS__) & 1))
+#define __RSE_READ__ ((BOOL)((__RSE_PIN__ >> __RSE__) & 1))
+
+#define __CTS_HIGH__ (__CTS_PORT__ |=  __CTS_BIT__)
+#define __RSE_HIGH__ (__RSE_PORT__ |=  __RSE_BIT__)
+
+#define __CTS_LOW___ (__CTS_PORT__ &= ~__CTS_BIT__)
+#define __RSE_LOW___ (__RSE_PORT__ &= ~__RSE_BIT__)
 
 enum E_BT_NUM
 {
-	EB_BT0 = 0xfe,
+	EB_BT0 = 0x00,
 	EB_BT1 = 0x01,
-	EB_BT2 = 0xfd,
-	EB_BT3 = 0x02,
-	EB_BT4 = 0xfb,
-	EB_BT5 = 0x04,
-	EB_BT6 = 0xf7,
-	EB_BT7 = 0x08,
-	EB_BT8 = 0xef,
-	EB_BT9 = 0x10,
-	EB_BTA = 0xdf,
-	EB_BTB = 0x20,
-	EB_BTC = 0xbf,
-	EB_BTD = 0x40,
-	EB_BTE = 0x7f,
-	EB_BTF = 0x80,
+	EB_BT2 = 0x02,
+	EB_BT3 = 0x03,
+	EB_BT4 = 0x04,
+	EB_BT5 = 0x05,
+	EB_BT6 = 0x06,
+	EB_BT7 = 0x07,
+	EB_BT8 = 0x08,
+	EB_BT9 = 0x09,
+	EB_BTA = 0x0a,
+	EB_BTB = 0x0b,
+	EB_BTC = 0x0c,
+	EB_BTD = 0x0d,
+	EB_BTE = 0x0e,
+	EB_BTF = 0x0f,
 };
+
+/************************************************************************/
 
 /**
  * \brief 
  * _arg_str_fと_arg_strを比較する 
- * _arg_strに_arg_str_fのすべてが含まれていればそれでいい
+ * _arg_strに_arg_str_fのすべてが含まれていれば一致となる
  *
- * \param _arg_str_f : 比較する文字列
- * \param _arg_str : 比較される文字列
+ * \param _arg_str_base : 比較する文字列
+ * \param _arg_str_comp : 比較される文字列
  * 
- * \return usint
- *		0 -> 一致
- *		1 -> 不一致
+ * \return BOOL : 比較の結果
+ *	不一致	-> FALSE
+ *	一致	-> TRUE
  */
-int strcmp (const char *_arg_str_f, char *_arg_str);
+BOOL F_Str_comp (const char _arg_str_base[], char _arg_str_comp[]);
 
-char * F_str_copy (char _arg_str_base[], const char _arg_str[]);
+/**
+ * \brief 
+ *	_arg_str_baseを_arg_strに書き換えます。
+ *	どちらかが、最後になるまで書き換えます。それ以降はそのまま。
+ *
+ * \param _arg_str_base	: 書き換えられる文字列
+ * \param _arg_str_copy	: 書き換える文字列
+ * 
+ * \return char * _arg_str_baseがreturnされます
+ */
+char * F_Str_copy (char _arg_str_base[], const char _arg_str_copy[]);
 
-void F_Set_bt (E_UART_ADDR _arg_uart_addr);
+/**
+ * \brief 
+ *	_arg_str_baseの後ろに_arg_str_addを追加します
+ * 
+ * \param _arg_str_base : 追加される文字列
+ * \param _arg_str_add  : 追加する文字列
+ */
+void F_Str_add(char _arg_str_base[], const char _arg_str_add);
 
-void F_Out_bt (E_UART_ADDR _arg_uart_addr_t, const char _arg_data_out[]);
+/**
+ * \brief 
+ *	Bluetoothへ_arg_data_outを送信する。
+ *	送信完了するまで続きます。プログラムが停止したら疑ってみてね。
+ *
+ * \param _arg_uart_addr_t	: 送信に用いるUART
+ * \param _arg_data_out		: 送信するdata
+ */
+void F_BT_Out (E_UART_ADDR _arg_uart_addr_t, const char _arg_data_out[]);
 
-char * F_In_bt (E_UART_ADDR _arg_uart_addr_r, char _arg_data_in[]);
+/**
+ * \brief 
+ *	Bluetoothから受信する。
+ *	受信したdataは_arg_re_data_inに格納される。
+ *	受信完了するまで続きます。プログラムが停止したら疑ってみてね。 
+ *
+ * \param _arg_uart_addr_r	: 受信に用いるUART
+ * \param _arg_re_data_in	: 受信したdataを格納する所
+ */
+void F_BT_In (E_UART_ADDR _arg_uart_addr_r, char _arg_re_data_in[]);
 
-char * F_In_bt (E_UART_ADDR _arg_uart_addr_r, char _arg_data_in[], const char _arg_data_comp[]);
+/**
+ * \brief 
+ *	受信したデータが_arg_data_compに一致するまで受信し続ける。
+ *	仕様上、合わなかったら無限ループ状態になるので気を付けてね。 
+ *
+ * \param _arg_uart_addr_r	: 用いるUART
+ * \param _arg_data_comp	: 比較対象
+ */
+void F_BT_In_comp (E_UART_ADDR _arg_uart_addr_r, const char _arg_data_comp[]);
 
-void F_In_comp_bt (E_UART_ADDR _arg_uart_addr_r, const char _arg_data_comp[]);
+/**
+ * \brief 
+ *	受信したデータが_arg_data_compに一致するまで受信し続ける。
+ *	受信したデータは_arg_re_data_inに格納される。
+ *	仕様上、合わなかったら無限ループ状態になるので気を付けてね。 
+ *
+ * \param _arg_uart_addr_r	: 用いるUART
+ * \param _arg_re_data_in	: 受信したデータを格納する場所
+ * \param _arg_data_comp	: 比較対象
+ */
+void F_BT_In_comp (E_UART_ADDR _arg_uart_addr_r, char _arg_re_data_in[], const char _arg_data_comp[]);
 
-#if defined(_AKILCD_H_)
+/**
+ * \brief 
+ *	Bluetoothをコマンドモードにする。
+ */
+void F_BT_Cut();
 
-char * F_Set_bt_addr (E_BT_NUM _arg_num_bt, char _arg_bt_addr[], char _arg_char_num);
+/************************************************************************/
 
-#else
-
-char * F_Set_bt_addr (E_BT_NUM _arg_num_bt, char _arg_bt_addr[]);
-
-#endif
-
-BOOL F_Connect_bt_slave (E_UART_ADDR _arg_uart_addr, E_BT_NUM _arg_num_bt);
-
-BOOL F_Connect_bt_master (E_UART_ADDR _arg_uart_addr, E_BT_NUM _arg_num_bt);
-
-void F_Re_Connect_bt_slave (E_UART_ADDR _arg_uart_addr, E_BT_NUM _arg_num_bt);
-
-void F_Re_Connect_bt_master (E_UART_ADDR _arg_uart_addr, E_BT_NUM _arg_num_bt);
-
-void F_Cut_Connect_bt();
+#include "H28_C_BT.cpp"
 
 #include "H28_BT.cpp"
 
-#include "H28_C_BT.cpp"
-#include "H28_C_BT_304.cpp"
-#include "H28_C_BT_304_MASTER.cpp"
-#include "H28_C_BT_304_SLAVE.cpp"
+
+//#include "H28_C_BT_304.cpp"
+//#include "H28_C_BT_304_MASTER.cpp"
+//#include "H28_C_BT_304_SLAVE.cpp"
